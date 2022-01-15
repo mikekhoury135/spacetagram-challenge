101bf669f95e339c0cb7d549a449734e9a0a8a4c
mikekhoury135 <mikekhoury135@gmail.com>
2022-01-15 05:15:58 -0500

    10 images now populated and for loop for doing that is mostly complete

--- assets/js/apod.js
+++ assets/js/apod.js
@@ -1,12 +1,7 @@
 //api-key
 var url = 'https://api.nasa.gov/planetary/apod?api_key=mDDTHtQpHkPKUJL8FtI4cOsiGm3Hr4paNKBbR59A&start_date=2022-01-05';
 
-// card structure
-
-// const card = {
-//     document.createElement('div');
-// };
-
+// main element declaration
 const mainEl = document.querySelector('main');
 
 //api fetch and json parse
@@ -16,39 +11,59 @@ fetch (url, {
 })
 .then(response => {
     response.json().then(function(data) {
-        
-        for (let i = 0; i < 10; i++) {
-            const template = document.createElement('template');
-            template.innerHTML = `
-            <div class="card col-6 offset-3 mt-5">
-                <img class="card-img-top img-fluid" id="space-img" alt="NASA's Astronomy Picture of the Day">
-                <div class="card-body">
-                    <h5 class="card-title" id="space-title"></h5>
-                    <h6 class="" id="space-date"></h6>
-                    <details>
-                        <summary>Description</summary>
-                        <p class="card-text" id="space-description"></p>
-                    </details>
-                </div>
-            </div>`
 
-            class SpaceCard extends HTMLElement{
-                constructor(){
-                    super();
-                    this.attachShadow({ mode: 'open'});
-                    this.shadowRoot.appendChild(template.content.cloneNode(true));
-                    this.shadowRoot.querySelector('#space-img').src = data[i].hdurl;   
-                    this.shadowRoot.querySelector('#space-title').innerText = data[i].title;
-                    this.shadowRoot.querySelector('#space-date').innerText = data[i].date;
-                    this.shadowRoot.querySelector('#space-description').innerText = data[i].explanation;
-                } 
-            }
-            window.customElements.get('space-card') || window.customElements.define('space-card', SpaceCard);
+        for (let i = 10; i > 0; i--) {
+            
+            // parent div
+            let div = document.createElement("div");
 
-            // document.getElementById('space-img').setAttribute('src', data[i].hdurl)
-            // document.getElementById('space-title').innerHTML = data[i].title;
-            // document.getElementById('space-date').innerHTML = data[i].date;
-            // document.getElementById('space-description').innerHTML = data[i].explanation;
+            // card image
+            let img = document.createElement("img");
+            img.setAttribute('src', data[i].hdurl);
+
+            // div for info under image
+            let template = document.createElement('div');
+            
+            // title for card
+            let title = document.createElement("h5");
+            title.innerHTML = data[i].title;
+            
+            // date for card
+            let date = document.createElement("h6");
+            
+            // dropdown for summary text
+            let details = document.createElement("details");
+
+            // summary for the image
+            let description= document.createElement("p");
+            
+
+
+            img.setAttribute("class", "card-img-top img-fluid");
+            
+            title.setAttribute("class", "card-title");
+            
+            date.innerHTML=data[i].date;
+            
+            description.innerHTML = data[i].explanation;
+            description.setAttribute("class", "card-text");
+            
+            details.appendChild(description);
+            
+            template.appendChild(title);
+            template.appendChild(date);
+            template.appendChild(details);
+            template.setAttribute("class", "card-body");
+
+            
+            div.appendChild(img);
+            div.appendChild(template);
+            div.setAttribute("class", "card col-6 offset-3 mt-5");
+            
+            mainEl.appendChild(div);
+
+            console.log(data[i]);
+           
         };
     });
 });
