d1fa1cf9efac014aa142b128aae0227e7e1e7716
mikekhoury135 <mikekhoury135@gmail.com>
2022-01-15 16:59:43 -0500

    favicon added, iframe cards functional, header structured

--- assets/js/apod.js
+++ assets/js/apod.js
@@ -11,15 +11,34 @@ fetch (url, {
 })
 .then(response => {
     response.json().then(function(data) {
-
+        
         for (let i = 10; i > 0; i--) {
             
             // parent div
             let div = document.createElement("div");
-
-            // card image
+            
+            // card image or video
             let img = document.createElement("img");
-            img.setAttribute('src', data[i].hdurl);
+            let iframe = document.createElement("iframe");
+            
+            //conditionl to determine if iframe or img tag is used
+            if (data[i].media_type === 'image') {
+                img.setAttribute('src', data[i].hdurl);
+                img.setAttribute("class", "card-img-top img-fluid");
+                div.appendChild(img);
+            } else if (data[i].media_type === 'video') {
+                div.appendChild(iframe);
+                iframe.setAttribute('width', '100%');
+                iframe.setAttribute('height', '300px');
+                iframe.setAttribute('src', data[i].url);
+                iframe.setAttribute('frameborder', '0');
+                iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+                iframe.setAttribute('allowfullscreen', '');
+                iframe.setAttribute('SameSite', 'None');
+            } else {
+                return null;
+            }
+
 
             // div for info under image
             let template = document.createElement('div');
@@ -27,43 +46,33 @@ fetch (url, {
             // title for card
             let title = document.createElement("h5");
             title.innerHTML = data[i].title;
+            title.setAttribute("class", "card-title");
             
             // date for card
             let date = document.createElement("h6");
+            date.innerHTML=data[i].date;
             
             // dropdown for summary text
             let details = document.createElement("details");
-
+            
             // summary for the image
             let description= document.createElement("p");
-            
-
-
-            img.setAttribute("class", "card-img-top img-fluid");
-            
-            title.setAttribute("class", "card-title");
-            
-            date.innerHTML=data[i].date;
-            
             description.innerHTML = data[i].explanation;
             description.setAttribute("class", "card-text");
-            
             details.appendChild(description);
             
+            //appending card content to their respective containers            
             template.appendChild(title);
             template.appendChild(date);
             template.appendChild(details);
             template.setAttribute("class", "card-body");
 
-            
-            div.appendChild(img);
             div.appendChild(template);
-            div.setAttribute("class", "card col-6 offset-3 mt-5");
+            div.setAttribute("class", "card col-8 offset-2 mt-3");
             
             mainEl.appendChild(div);
 
             console.log(data[i]);
-           
         };
     });
 });
