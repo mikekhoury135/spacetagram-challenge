2242749e6e2ebccfc181166e47aa16e2a64ba327
mikekhoury135 <mikekhoury135@gmail.com>
2022-01-17 00:46:36 -0500

    added some css and currently debugging an api timeout issue

--- assets/js/apod.js
+++ assets/js/apod.js
@@ -45,7 +45,7 @@ function api(dateFormat) {
     .then(response => {
         response.json().then(function(data) {
             
-            for (let i = 10; i > 0; i--) {
+            for (let i = 0; i < 10; i++) {
                 
                 // parent div
                 let div = document.createElement("div");
